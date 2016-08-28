27dd2a55d6f517e8964258e013e57459462336e4
ramkin85 <ramkin85@yandex.ru>
2016-08-28 15:08:12 +0300

    initial commit

--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
 __author__ = 'ramkin85'
-from app import app, trainer, client
+from app import app
 from flask import render_template, request, json
 from app.client import api as client_api
-from trainer import api as trainer_api
+from app.trainer import api as trainer_api
 from app.lesson import api as lesson_api
 
 from app import lesson
