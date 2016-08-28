265e9f1ffd3955e6f47faca13f27c529ff3397ef
ramkin85 <ramkin85@yandex.ru>
2016-08-28 18:49:50 +0300

    initial commit

--- app/db_helper.py
+++ app/db_helper.py
@@ -8,12 +8,12 @@ import os.path
 
 def create_db():
     v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
-    app.logger.debug('v = %s' % v)
+    app.logger.debug('v is None = %s' % v is None)
     if v is None:
         db.create_all()
-    if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
-        api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
-        api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
-    else:
-        api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
+        if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
+            api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
+            api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+        else:
+            api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
     app.logger.debug('DB CREATED')
