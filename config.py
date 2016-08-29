293aa58272c4ebaa3bdda574d7bb6acdb9ec1801
ramkin85 <ramkin85@yandex.ru>
2016-08-29 22:02:43 +0300

    initial commit

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'postgres://ebuhqiuztdjgdi:KeaaSiMlbh3_Mpb36u7igmcZz_@ec2-54-235-87-70.compute-1.amazonaws.com:5432/dbqf5pv9nomngb'
+SQLALCHEMY_DATABASE_URI = 'jdbc:postgresql://ec2-54-235-87-70.compute-1.amazonaws.com:5432/dbqf5pv9nomngb?sslmode=require&user=ebuhqiuztdjgdi&password=KeaaSiMlbh3_Mpb36u7igmcZz_'
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
